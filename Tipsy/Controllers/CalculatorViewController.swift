e1b36f29b1189ff3943bcc49cddf90522dea293a
Juan Camilo Victoria <144855187+Juanaco123@users.noreply.github.com>
2024-05-27 10:58:56 -0500

    add calculate bill logic, Linking Screens.

--- Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class CalculatorViewController: UIViewController {
+    var tip = 0.1
+    var numberOfPeople = 2
+    var billTotal = 0.0
+    var finalResult = "0.0"
     
     @IBOutlet weak var billTextField: UITextField!
     @IBOutlet weak var zeroPctButton: UIButton!
@@ -16,32 +20,49 @@ class CalculatorViewController: UIViewController {
     @IBOutlet weak var twentyPctButton: UIButton!
     @IBOutlet weak var splitNumberLabel: UILabel!
     
-    var tip = 0.0
-    
     @IBAction func tipChanged(_ sender: UIButton) {
+        billTextField.endEditing(true)
         zeroPctButton.isSelected = false
         tenPctButton.isSelected = false
         twentyPctButton.isSelected = false
         
         sender.isSelected = true
-        
         let buttonTitle = sender.currentTitle!
-        
         let buttonTitleMinusPercentSign = String(buttonTitle.dropLast())
-        
         let buttonTitleAsDouble = Double(buttonTitleMinusPercentSign)!
-        
         tip = buttonTitleAsDouble / 100
         
     }
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
-        sender.minimumValue = 2.0
-        let splitNumberAsInt = Int(sender.value)
-        splitNumberLabel.text = String(splitNumberAsInt)
-    }
-    @IBAction func calculatePressed(_ sender: UIButton) {
-        print(tip)
+        billTextField.endEditing(true)
+        splitNumberLabel.text = String(format: "%.0f", sender.value)
+        numberOfPeople = Int(sender.value)
     }
     
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        let bill = billTextField.text!
+        
+        if bill != "" {
+            
+            billTotal = Double(bill)!
+            
+            let result = billTotal * (1 + tip) / Double(numberOfPeople)
+            
+            finalResult = String(format: "%.2f", result)
+            
+        }
+        self.performSegue(withIdentifier: "goToResults", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goToResults" {
+            let destinationVC = segue.destination as! ResultsViewController
+            
+            destinationVC.result = finalResult
+            destinationVC.tip = Int(tip * 100)
+            destinationVC.split = numberOfPeople
+            
+        }
+    }
 }
 
